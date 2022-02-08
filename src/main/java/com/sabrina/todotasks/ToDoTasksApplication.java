e8106c515d54c587c267b5b04306378f680edd30
SabrinaPVaz <88401720+SabrinaPVaz@users.noreply.github.com>
2022-02-08 09:26:48 -0300

    Initial commit

---
+++ src/main/java/com/sabrina/todotasks/ToDoTasksApplication.java
@@ -0,0 +1,13 @@
+package com.sabrina.todotasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoTasksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoTasksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sabrina/todotasks/ToDoTasksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sabrina.todotasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoTasksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
